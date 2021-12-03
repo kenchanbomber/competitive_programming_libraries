5844eccf5de021f26d3905d947a22eaa5e8b45f5
kengo hirata <athlete551@icloud.com>
2021-12-04 02:01:42 +0900

    add prime_factorization

---
+++ prime_factorization.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+map<ll,int> prime_factorization(ll n) {
+    map<ll,int> mp;
+    for(ll i = 2; i*i <= n; ++i) while(n%i == 0){ mp[i]++; n /= i; }
+    if(n > 1) mp[n]++;
+    return mp;
+}
+
+int main() {
+    ll n;
+    cin >> n;
+    map<ll,int> primes = prime_factorization(n);
+    for(auto &p : primes) printf("prime: %lld, cnt: %d\n", p.first, p.second);
+    return 0;
+}
