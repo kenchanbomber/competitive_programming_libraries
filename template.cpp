1b5ba824296453f0030882e7e457509531870674
kengo hirata <athlete551@icloud.com>
2021-12-21 21:14:40 +0900

    add template to dx, dy

--- template.cpp
+++ template.cpp
@@ -6,10 +6,10 @@ using T = tuple<int, int, int>;
 using vi = vector<int>;
 using vvi = vector<vector<int>>;
 #define rep(i, j, n) for (int i = j; i < (int)(n); i++)
-template <typename T>
-bool chmax(T &a, const T &b) { if(a < b) { a = b; return true; } return false; }
-template <typename T>
-bool chmin(T &a, const T &b) { if(a > b) { a = b; return true; } return false; }
+template <typename T>bool chmax(T &a, const T &b) { if(a < b) { a = b; return true; } return false; }
+template <typename T>bool chmin(T &a, const T &b) { if(a > b) { a = b; return true; } return false; }
+const vector<int> dx = {-1,0,1,0};
+const vector<int> dy = {0,1,0,-1};
 
 int main() {
     return 0;
