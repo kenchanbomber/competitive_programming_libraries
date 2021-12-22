cc068ec4bb92bf3b6aafbf49e631f4b6e375fec0
kengo hirata <athlete551@icloud.com>
2021-12-22 09:04:31 +0900

    自動でmodをとる64bit整数型mintを追加#2

---
+++ number_theory/mint.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int mod = 1000000007;
+
+struct mint {
+    long long x;
+    mint(long long x = 0) :x((x% mod + mod) % mod) {}
+    mint operator-() const { return mint(-x); }
+    mint& operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator-=(const mint a) {
+        if ((x += mod - a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this; }
+    mint operator+(const mint a) const { return mint(*this) += a; }
+    mint operator-(const mint a) const { return mint(*this) -= a; }
+    mint operator*(const mint a) const { return mint(*this) *= a; }
+    mint pow(long long t) const {
+        if (!t) return 1;
+        mint a = pow(t >> 1);
+        a *= a;
+        if (t & 1) a *= *this;
+        return a;
+    }
+    // for prime mod
+    mint inv() const { return pow(mod - 2); }
+    mint& operator/=(const mint a) { return *this *= a.inv(); }
+    mint operator/(const mint a) const { return mint(*this) /= a; }
+};
+
+int main() {
+    mint a = -1;
+    mint b = 1000000010;
+    cout << a.x << "\n"; // 1000000006
+    cout << b.x << "\n"; // 3
+    return 0;
+}
