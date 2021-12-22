a4cf18cc11d1be9845bcfe5121d906a3e351d099
kengo hirata <athlete551@icloud.com>
2021-12-22 15:43:24 +0900

    templateにrep, srep, fi seを追加

--- template.cpp
+++ template.cpp
@@ -5,7 +5,10 @@ using P = pair<int, int>;
 using T = tuple<int, int, int>;
 using vi = vector<int>;
 using vvi = vector<vector<int>>;
-#define rep(i, j, n) for (int i = j; i < (int)(n); i++)
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define srep(i, j, n) for (int i = j; i < (int)(n); i++)
+#define fi first
+#define se second
 template <typename T>bool chmax(T &a, const T &b) { if(a < b) { a = b; return true; } return false; }
 template <typename T>bool chmin(T &a, const T &b) { if(a > b) { a = b; return true; } return false; }
 const vector<int> dx = {-1,0,1,0};
